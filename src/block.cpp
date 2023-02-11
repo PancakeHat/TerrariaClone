ed5487c35c9bd23781f79e8bf69b93a2a73547e9
PancakeHat <101941509+PancakeHat@users.noreply.github.com>
2023-02-11 12:38:56 -0600

    Added collision, jumping, world

--- src/block.cpp
+++ src/block.cpp
@@ -2,6 +2,7 @@
 #include <SDL_image.h>
 #include <Windows.h>
 #include "vectors.h"
+#include <iostream>
 
 class Block
 {
@@ -9,14 +10,40 @@ public:
 	Vector2 position;
 	Uint8 blockType;
 
-	void RenderBlock(Vector2 camPosition, SDL_Rect blockRect, SDL_Renderer* renderer)
+	bool IsColliding(Vector2 playerPosition)
 	{
-		SDL_Rect* tempRect = &blockRect;
-		tempRect->x = position.x;
-		tempRect->y = position.y;
+		// if we line up on the y axis
+		if(((playerPosition.y >= position.y) && ((playerPosition.y) <= (position.y + 40)) || (((playerPosition.y + 90) >= position.y) && ((playerPosition.y + 90) <= (position.y + 40)) || ((playerPosition.y + 45) >= position.y) && (playerPosition.y + 45) <= (position.y + 40))))
+		{
+			// if we line up on the x axis
+			if (((playerPosition.x >= position.x) && ((playerPosition.x) <= (position.x + 40)) || (((playerPosition.x + 40) >= position.x) && ((playerPosition.x + 40) <= (position.x + 40)))))
+			{
+				return true;
+			}
+		}
 
-		SDL_SetRenderDrawColor(renderer, 0, 128, 0, 255);
-		SDL_RenderFillRect(renderer, tempRect);
+		return false;
+	}
+
+	void RenderBlock(Vector2 camPosition, SDL_Rect blockRect, SDL_Renderer* renderer, Vector2 playerPos)
+	{
+		if (blockType != 0)
+		{
+			SDL_Rect* tempRect = &blockRect;
+			tempRect->x = ((int)position.x - (int)camPosition.x);
+			tempRect->y = ((int)position.y - (int)camPosition.y);
+
+			SDL_SetRenderDrawColor(renderer, 0, 128, 0, 255);
+			SDL_RenderFillRect(renderer, tempRect);
+
+			tempRect->x = ((int)position.x + 5 - (int)camPosition.x);
+			tempRect->y = ((int)position.y + 5 - (int)camPosition.y);
+			tempRect->w = 30;
+			tempRect->h = 30;
+
+			SDL_SetRenderDrawColor(renderer, 0, 96, 0, 255);
+			SDL_RenderFillRect(renderer, tempRect);
+		}
 
 	}
 };
@@ -26,4 +53,10 @@ class BlockCollum
 public:
 	int x;
 	Block blocks[16];
+};
+
+class World
+{
+public:
+	BlockCollum blockCollums[32];
 };
--- src/main.cpp
+++ src/main.cpp
@@ -17,10 +17,9 @@
 SDL_Renderer* renderer;
 SDL_Window* window;
 bool isRunning;
-bool fullscreen;
+bool fullscreen = false;
 
-Block testBlock;
-BlockCollum testCollum;
+World world;
 
 SDL_Rect blockRectDefault;
 
@@ -28,9 +27,14 @@ void Render();
 void HandleFPS();
 void Update();
 void Input();
+void GenerateWorld();
+void Jump();
 
 int frameCount, timerFPS, lastFrame, fps;
 static int lastTime;
+
+bool collisionDirections[4];
+bool jumping = false;
 
 Vector2 cameraPosition;
 
@@ -41,20 +45,26 @@ Vector2 playerPosition;
 SDL_Rect testRect;
 SDL_Texture* testTexture;
 
+bool hasCheckedCollisionForFrame = false;
+int iTest = 0;
+int jumpVelocity = 0;
+
 void GameInit()
 {
 	playerPosition.x = (WINDOW_WIDTH / 2) - 20;
-	playerPosition.y = (WINDOW_HEIGHT / 2) - 45;
-
-	testBlock.position.x = 80;
-	testBlock.position.y = 80;
+	playerPosition.y = ((WINDOW_HEIGHT / 2) - 45) + 5;
 
 	blockRectDefault.w = 40;
 	blockRectDefault.h = 40;
-	testCollum.x;
 
 	cameraPosition.x = 0;
 	cameraPosition.y = 0;
+
+
+	for (int i = 0; i < 4; i++)
+	{
+		collisionDirections[i] = false;
+	}
 
 	fullscreen = false;
 	int flags = 0;
@@ -79,11 +89,34 @@ void GameInit()
 			isRunning = true;
 		}
 		std::cout << "Game has been started, we are ready to roll!\n";
+		std::cout << "\nControls:\n'SPACE' - Jump\n'A' - Move left\n'D' - Move right\n'Up/Down/Left/Right - NoClip in any direction\n";
 	}
 
 	SDL_Surface* tmpSurface = IMG_Load("assets/test.png");
 	testTexture = SDL_CreateTextureFromSurface(renderer, tmpSurface);
 	SDL_FreeSurface(tmpSurface);
+
+	GenerateWorld();
+}
+
+int randint(int MAX_NUMBER)
+{
+	srand(time(0));
+	return (rand() % MAX_NUMBER);
+}
+
+void GenerateWorld()
+{
+	for (int i = 0; i < 32; i++)
+	{
+		world.blockCollums[i].x = (i * 40);
+		for (int k = 0; k < 5; k++)
+		{
+			world.blockCollums[i].blocks[k].position.y = (k * 40) + 640;
+			world.blockCollums[i].blocks[k].position.x = world.blockCollums[i].x;
+			world.blockCollums[i].blocks[k].blockType = 1;
+		}
+	}
 }
 
 int main() {
@@ -91,10 +124,10 @@ int main() {
 
 	// update loop
 	while (isRunning) {
-		Input();
 		Update();
 		HandleFPS();
 		Render();
+		Input();
 
 		int timerFPS = SDL_GetTicks() - lastFrame;
 		if (timerFPS < (1000 / 60))
@@ -112,23 +145,93 @@ int main() {
 	return 0;
 }
 
+void CheckPlayerCollisions(Block blockToCheck)
+{
+	/*
+	0 - no moving left
+	1 - no moving right
+	2 - no moving down
+	3 - no moving up
+	*/
+
+	if (playerPosition.x >= blockToCheck.position.x)
+	{	
+		if(!(playerPosition.y + 90 <= blockToCheck.position.y + 5))
+			if (!(playerPosition.y >= blockToCheck.position.y - 5))
+				collisionDirections[0] = true;
+	}
+
+	if (playerPosition.x + 20 <= blockToCheck.position.x)
+	{
+		if (!(playerPosition.y + 90 <= blockToCheck.position.y + 5))
+			if (!(playerPosition.y >= blockToCheck.position.y - 5))
+				collisionDirections[1] = true;
+	}
+
+	if(playerPosition.y <= blockToCheck.position.y)
+	{
+		collisionDirections[2] = true;	
+	}
+
+	if (playerPosition.y >= blockToCheck.position.y + 40)
+	{
+		collisionDirections[3] = true;	
+	}
+
+	//if ((playerPosition.x + 20 >= blockToCheck.position.x) && ((playerPosition.x + 20) <= (blockToCheck.position.x + 40)))
+
+}
+
 // render
 void Render() {
+
+	hasCheckedCollisionForFrame = false;
 
 	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
 	SDL_RenderClear(renderer);
 
 	// draw here
 	//SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+
+
+	collisionDirections[0] = false;
+	collisionDirections[1] = false;
+	collisionDirections[2] = false;
+	collisionDirections[3] = false;
+
 	SDL_Rect rect;
-	rect.x = playerPosition.x;
-	rect.y = playerPosition.y;
+	rect.x = playerPosition.x - cameraPosition.x;
+	rect.y = playerPosition.y - cameraPosition.y;
 	rect.w = 40;
 	rect.h = 90;
 	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
 	SDL_RenderFillRect(renderer, &rect);
+	/* Add corners to the player
+	rect.w = 10;
+	rect.h = 10;
+	SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+	SDL_RenderFillRect(renderer, &rect);
+	rect.y = (playerPosition.y + 80) - cameraPosition.y;
+	rect.w = 10;
+	rect.h = 10;
+	SDL_SetRenderDrawColor(renderer, 255, 0, 255, 255);
+	SDL_RenderFillRect(renderer, &rect);
+	*/
 
-	testBlock.RenderBlock(cameraPosition, blockRectDefault, renderer);
+	for (BlockCollum blockCollum : world.blockCollums)
+	{
+		for (Block block : blockCollum.blocks)
+		{
+			block.RenderBlock(cameraPosition, blockRectDefault, renderer, playerPosition);
+			if (block.IsColliding(playerPosition))
+			{
+				CheckPlayerCollisions(block);
+				//std::cout << "Collision on frame " << iTest << "; Block :: (" << block.position.x << ", " << block.position.y << ")\n";
+				//std::cout << "Coll :: " << collisionDirections[0] << ", " << collisionDirections[1] << ", " << collisionDirections[2] << ", " << collisionDirections[3] << "\n";
+			}
+		}
+	}
+
 
 	//SDL_RenderCopy(renderer, testTexture, NULL, &rect);
 
@@ -136,8 +239,32 @@ void Render() {
 }
 
 // update (stuff that happens every frame)
-void Update() {
+void Update() 
+{
+	iTest++;
+	//std::cout << "Coll :: " << collisionDirections[0] << ", " << collisionDirections[1] << ", " << collisionDirections[2] << ", " << collisionDirections[3] << "\n";
+	Jump();
+}
 
+void Jump()
+{
+	if (jumping)
+	{
+		if (jumpVelocity < 20)
+		{
+			jumpVelocity++;
+			playerPosition.y -= 20 - jumpVelocity;
+			cameraPosition.y -= 20 - jumpVelocity;
+		}
+		else if (jumpVelocity >= 20 && jumpVelocity < 25)
+		{
+			jumpVelocity++;
+			playerPosition.y += jumpVelocity - 10;
+			cameraPosition.y += jumpVelocity - 10;
+		}
+		else if(jumpVelocity >= 25)
+			jumping = false;
+	}
 }
 
 // called every frame to get input
@@ -156,29 +283,62 @@ void Input()
 	}
 
 	// get input
-	if (GetKeyState('W') & 0x8000)
+	if (GetKeyState(VK_SPACE) & 0x8000 && collisionDirections[2])
+	{
+		if (!jumping)
+		{
+			jumpVelocity = 0;
+			jumping = true;
+		}
+	}
+
+	if (!collisionDirections[2] && !(GetKeyState(VK_UP) & 0x8000) && !jumping)
+	{
+		playerPosition.y += 10;
+		cameraPosition.y += 10;
+	}
+	/*
+	if (GetKeyState('S') & 0x8000 && !collisionDirections[2])
+	{
+		playerPosition.y += 5;
+		cameraPosition.y += 5;
+	}
+	*/
+		
+	if (GetKeyState('D') & 0x8000 && !collisionDirections[1])
+	{
+		playerPosition.x += 5;
+		cameraPosition.x += 5;
+	}
+		
+	if (GetKeyState('A') & 0x8000 && !collisionDirections[0])
+	{
+		playerPosition.x -= 5;
+		cameraPosition.x -= 5;
+	}
+
+	if (GetKeyState(VK_UP) & 0x8000)
 	{
 		playerPosition.y -= 5;
 		cameraPosition.y -= 5;
 	}
-	if (GetKeyState('S') & 0x8000)
-		playerPosition.y += 5;
-	if (GetKeyState('D') & 0x8000)
-		playerPosition.x += 5;
-	if (GetKeyState('A') & 0x8000)
-		playerPosition.x -= 5;
-	
-	/*
-	// temp
-	if (GetKeyState(VK_UP) & 0x8000)
-		testBlock.position.y -= 40;
 	if (GetKeyState(VK_DOWN) & 0x8000)
-		testBlock.position.y += 40;
+	{
+		playerPosition.y += 5;
+		cameraPosition.y += 5;
+	}
+
 	if (GetKeyState(VK_RIGHT) & 0x8000)
-		testBlock.position.x += 40;
+	{
+		playerPosition.x += 5;
+		cameraPosition.x += 5;
+	}
+
 	if (GetKeyState(VK_LEFT) & 0x8000)
-		testBlock.position.x -= 40;
-	*/
+	{
+		playerPosition.x -= 5;
+		cameraPosition.x -= 5;
+	}
 	
 }
 
